06d23a240919134da9eb03c8f96941c8248fc41c
2005869 <2005869@aluno.univesp.br>
2023-04-07 10:12:53 -0300

    read level of water and send to serial

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,27 @@
 #include <Arduino.h>
 
+int sensorPin = A0;
+int digitalPin = D1;
+
+int level = 0;
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  pinMode(digitalPin, OUTPUT);
+  digitalWrite(digitalPin, LOW);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  digitalWrite(digitalPin, HIGH);
+  delay(1000);
+  level = analogRead(sensorPin);
+  digitalWrite(digitalPin, LOW);
+  Serial.print("Level of water: ");
+  Serial.println(level);
+  delay(10000);
 }
